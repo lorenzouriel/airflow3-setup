73764ae7ae0caddc3bc6cff80052a077d3fb832f
Lorenzo Uriel <lorenzo.uriel@geosiga.com.br>
2025-10-02 18:59:34 -0300

    feat: course DAG Authoring from AirFlow started

---
+++ setup-basic-with-dags-examples/dags/dynamic_task_mapping/expand.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from airflow.decorators import task
+
+from datetime import datetime
+import random
+
+with DAG(
+    dag_id="expand",
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["example"],
+) as dag:
+
+    @task
+    def get_files():
+        return ["file_{nb}" for nb in range(random.randint(3, 5))]
+
+    @task
+    def download_files(file: str):
+        print(f"Downloading {file}")
+    
+    files = download_files.expand(file=get_files())
